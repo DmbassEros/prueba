d42676b8fcc49643ec80370bc531dedc80eb262e
Enzo Eros Calvino <enzo-eros@hotmail.com>
2023-10-09 12:50:08 +0200

    Modificacion de A

--- prueba.cs
+++ prueba.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Test de fuerza de ramaB");
+        Console.WriteLine("Test de estrés de ramaA");
+        Console.WriteLine("Test de fuerza de ramaA");
     }
 }
