eb83286589dbb7ed468f776cbe6b0b2ec48c11d4
Enzo Eros Calvino <enzo-eros@hotmail.com>
2023-10-09 13:33:13 +0200

    prueba vacia

--- prueba.cs
+++ prueba.cs
@@ -4,9 +4,5 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Test de estrés de ramaA");
-        Console.WriteLine("Test de fuerza de ramaA");
-        Console.WriteLine("Test de fuerza de ramaB");
-        Console.WriteLine("Test de estrés de ramaB");
     }
 }
