535cf4b9563313594ebb92ed8ce1fb70096daea8
Enzo Eros Calvino <enzo-eros@hotmail.com>
2023-10-09 13:37:44 +0200

    edicion desarrollador_2

--- prueba.cs
+++ prueba.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
-		Console.WriteLine("Test de estrés Desarrollador 1");
+		Console.WriteLine("Test de estrés Desarrollador 2");
+		Console.WriteLine("Test de fuerza Desarrollador 2");
     }
 }
